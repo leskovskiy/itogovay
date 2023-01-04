38e43285f41df844d4a725960e6b5f25c7c598fd
Olesya Upit <olesyaupit@MacBook-Air-Olesya.local>
2023-01-04 17:39:57 +0700

    add gitignore

--- zadacha/Program.cs
+++ zadacha/Program.cs
@@ -1,1 +1,22 @@
 ﻿
+string[] array = {"57484", "world", "1", "puk"};
+string[] NewArray = new string[array.Length];
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        NewArray[count] = array[i];
+        count++;
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(NewArray);
